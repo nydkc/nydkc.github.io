15b903c02556d9f25eeb1e8dbe8df45ae99ec4da
Dante Gil-Marin <dantmari@voorheesville.org>
2017-11-14 22:55:56 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $(".dropdown-button").dropdown();
+
+
+  }); // end of document ready
+
+  $('.collapsible').collapsible();
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+})(jQuery); // end of jQuery name space
